abf46559297af2b124509b84dcb24aad81514ce1
janjantjan <janjan.tjan@gmail.com>
2017-02-12 21:28:11 -0500

    Almost there. Compiles

--- QueenBoard.java
+++ QueenBoard.java
@@ -5,24 +5,139 @@ public class QueenBoard{
 
     public QueenBoard(int size){
 	board = new int[size][size];
-    }
-
-    public boolean solve()
-    {   for (int i = 0; i < board.length; i++){
-	    for (int j = 0; j< board[0].length, j++){
+	for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j< board[0].length; j++){
 		board[i][j]=0;
 	    }
 	}
+    }
+
+    public boolean solve()
+    {   
 	return solveH(0);
     }
 
+    public boolean issolved(){
+	int q = 0;
+	for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j< board.length; j++){
+		if (board[i][j]==-1){q++;}
+	    }
+	}
+	return (q==board.length);
+    }
+
+       
+    private boolean addQueen(int c, int r){
+	if (r >= board.length){return false;}
+	if (board[r][c]== 0){
+	   	   
+	    for(int x=0;x<board.length;x++){
+		board[x][c]++;
+	   }
+	    
+	    for(int y=0;y<board.length;y++){
+		board[r][y]++;
+	    }
+	    int p = 0;//y
+	    int q = board.length-1;//x
+	    if (r+c-board.length >= 0){
+		 p = (r+c) - board.length+1;}
+	    else {q = r+c;}
+	    while ((p <= board.length-1) && (q>= 0)){
+		board[p][q]++;
+		p++;
+		q--;
+	    }
+
+	    int s = board.length-1;//y
+	    int t = board.length-1;//x
+	    if (c>=r){
+	        s = board.length + r - c - 1;}
+	    else {t = board.length - r + c - 1 ;}
+	    while ((s >= 0) && (t >= 0)){
+		board[s][t]++;
+		s--;
+		t--;
+	    }
+	
+	    board[r][c]=-1;
+	    
+	    return true;
+	}
+	return addQueen(c, r+1);
+    }				     
+
+         
+    private boolean removeQueen(int c){
+	for (int r = 0; r < board.length; r++){
+	    board[r][c]--;
+	    if (board[r][c]== -1){
+		for(int x=0; x<board.length;x++){
+		    board[r][x]--; 
+		}
+		int p = 0;//y
+		int q = board.length-1;//x
+		if (r+c-board.length >= 0){
+		    p = (r+c) - board.length+1;}
+		else {q = r+c;}
+		while ((p <= board.length-1) && (q>= 0)){
+		    board[p][q]++;
+		    p++;
+		    q--;
+		}
+
+		int s = board.length-1;//y
+		int t = board.length-1;//x
+		if (c>=r){
+		    s = board.length + r - c - 1;}
+		else {t = board.length - r + c - 1 ;}
+		while ((s >= 0) && (t >= 0)){
+		    board[s][t]++;
+		    s--;
+		    t--;
+		}
+		board[r][c]= 0;
+		return true;
+	    }
+	 
+	}
+	return false;
+    }	
+	
+	
+          	
     private boolean solveH(int col){
-	if (issolved()){return true;}
-	if (col > board.length){ return false;}
-	if (addqueen(col)){
-	    addqueen(col);
+	if (col >= board.length){
+	    return issolved();}
+	
+	if (addQueen(col,0)){
+	    addQueen(col,0);
 	    return solveH(col+1);}
-	if solve(solveH(s+1)){return true;}
+	if (solveH(col+1)){return true;}
+	return solveH(col+1);
+    }
 
+    public String toString(){
+	String fin = "";
+	 if (solve()){
+	for (int i = 0; i < board.length ; i++) {
+	    for (int j=0; j < board.length; j++) {
+		if (board[i][j] == -1) {
+		    fin += "Q" + "  ";
+		}
+		else {fin += "_" + "  ";}
+	    }
+	    fin += "\n";}
+	return fin ;
+	}
+	 return "no sol";
+    }
 
+    public static void main (String[]args){
+	QueenBoard q = new QueenBoard(5);
+	q.solve();
+	System.out.println(q.toString());
+    }
+}
 
