82eea6cef7904a37d723bee54c5c47a733400ef0
janjantjan <janjan.tjan@gmail.com>
2017-02-20 21:27:03 -0500

    Does compile. Does not run correctly... work on SolveH

--- 03/KnightBoard.java
+++ 03/KnightBoard.java
@@ -52,15 +52,101 @@ public class KnightBoard {
 
 	if (board[row][col]==0){
 	    board[row][col] = level;
+
+	     if ((row < 2) && (col > (board.length-2))) {
+		for (int a = 3; a > 0; a-=2){
+		    int y = nexRow[a];
+		    int x = nexCol[a];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+
+	    else if ((row > (board.length-2))&&(col >(board.length-2))){
+		for (int c = 7; c > 4; c-=2){
+		    int y = nexRow[c];
+		    int x = nexCol[c];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+
+	    else if (row > (board.length-2)){
+		for (int b = 7; b >= 1; b=2){
+		    int y = nexRow[b];
+		    int x = nexCol[b];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+
+	    else if ((row < 2)&&(col < 2)){
+		for (int g = 2; g >= 0; g-=2){
+		    int y = nexRow[g];
+		    int x = nexCol[g];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+
 	    
-	    for (int i = 0; i < nexRow.length; i++){
-		int r = nexRow[i];
-		int c = nexCol[i];	    
-		if (solveH(row+r, col+c, level+1)){
+	    else if (col < 2){
+		for (int h = 3; h >= 0; h--){
+		    int y = nexRow[h];
+		    int x = nexCol[h];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+
+	    else if ((row > board.length-2)&&(col < 2)){
+		for (int e = 6; e >=1 ; e-=5){
+		    int y = nexRow[e];
+		    int x = nexCol[e];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+
+	    
+	    else if (row > board.length- 2){
+		for (int f = 6; f >= 0; f-=2){
+		    int y = nexRow[f];
+		    int x = nexCol[f];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+
+	    else if ((row < 2)&&(col < 2)){
+		for (int g = 2; g < 0; g-=2){
+		    int y = nexRow[g];
+		    int x = nexCol[g];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+	    
+
+	    else if ((row < 2)&&(col < 2)){
+		for (int d = 7; d >= 4; d--){
+		    int y = nexRow[d];
+		    int x = nexCol[d];	    
+		    if (solveH(row+y, col+x, level+1)){
+			return true;}
+		}
+	    }
+	   
+	   
+	    else for (int i = 0; i < nexRow.length; i++){
+		int y = nexRow[i];
+		int x = nexCol[i];	    
+		if (solveH(row+y, col+x, level+1)){
 		    return true;}
 	    }
 	}
 	return false;}
+
    
     public String toString(){
 	String fin = "";
