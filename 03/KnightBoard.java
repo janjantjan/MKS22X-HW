5abd16acc384b1cc87cff14675279150d80c62f5
janjantjan <janjan.tjan@gmail.com>
2017-02-27 22:57:21 -0500

    Works (up to not including 7X7)

--- 03/KnightBoard.java
+++ 03/KnightBoard.java
@@ -15,7 +15,7 @@ public class KnightBoard {
 
 
     public void solve() {
-	solveH(1,0,1);
+	solveH(0,0,1);
 	
     }
 
@@ -26,10 +26,10 @@ public class KnightBoard {
 	
     private boolean solveH(int row , int col , int level){
          
-	if((board.length * board[0].length)<=level){
+	if((board.length * board[0].length)<level){
 	    return true;}
 
-	if (row<0 || row >= board.length || col<0 || col>= board[0].length){return false;}
+	 if (row<0 || row >= board.length || col<0 || col>= board[0].length){return false;}
 	    
        
 	if (board[row][col]==0){
@@ -43,49 +43,48 @@ public class KnightBoard {
 		
 	    }
        
-	     if(row > 1 && col < board.length ){
+	    if(row > 1 && col < board[0].length ){
                	if(solveH(row-2,col+1,level+1)){
 
 		    return true;}
-	     }
+	    }
        
-	     if(row <  board.length -2 && col <  board.length){
+	    if(row <  board.length -2 && col <  board[0].length){
                	if(solveH(row+2,col+1,level+1)){
 		    return true;}
-	     }
+	    }
         
-	     if(row <  board.length -2 && col > 0){
+	    if(row <  board.length -2 && col > 0){
 		if(solveH(row+2,col-1,level+1)){
 
 		    return true;}
-	     }
+	    }
         
-	     if(row > 1 && col > 0){
-		if(solveH(row-1,col-2, level+1))
-		    {
-			return true;}
-	     }
-        
-	     if(row > 1 && col <  board.length -1){
-               	if(solveH(row+1,col-2,level+1)){
+	    if(col > 1 && row > 0){
+		if(solveH(row-1,col-2, level+1)){
 		    return true;}
-	     }
-        
-	     if(row <  board.length -2 && col > 0){
-                if(solveH(row-1,col+2,level+1)){
-		    return true;}
-	     }
-        
-	     if(row <  board.length -2 && col <  board.length -1){
-                if(solveH(row+1,col+2,level+1)){
-		    return true;}
-
+	    }
 	
-		    
-	    
-	     }
+            
+	    if(col > 1 && row <  board.length -1){
+		if(solveH(row+1,col-2,level+1)){
+		    return true;}
+	    }
+        
+	    if(col <  board[0].length -2 && row > 0){
+		if(solveH(row-1,col+2,level+1)){
+		    return true;}
+	    }
+        
+	    if(col <  board[0].length -2 && row <  board.length -1){
+		if(solveH(row+1,col+2,level+1)){
+		    return true;}
+	    }
+
+	    board[row][col]=0;       
 	}
-    
+	 
+   
 	return false;
     }
 
@@ -111,16 +110,11 @@ public class KnightBoard {
 	KnightBoard k = new KnightBoard(5,6);
 	k.solve();
 	System.out.println(k.toString());
-	KnightBoard l = new KnightBoard(8,8);
-	l.solve();
-	System.out.println(l.toString());
 
-	KnightBoard n = new KnightBoard(7,7);
-	n.solve();
-	System.out.println(n.toString());
-	KnightBoard o = new KnightBoard(11,11);
-	o.solve();
-	System.out.println(o.toString());
+	
+	 KnightBoard o = new KnightBoard(6,6);
+	 o.solve();
+        System.out.println(o.toString());
 
 	
 
