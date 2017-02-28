48243f255756ec263e8b1d53754a80c98a67f0a8
janjantjan <janjan.tjan@gmail.com>
2017-02-27 21:40:52 -0500

    Works up to 13 by 13

--- 03/KnightBoard.java
+++ 03/KnightBoard.java
@@ -1,8 +1,7 @@
 public class KnightBoard {
     private int[][] board;
    
-    private int[] nexRow;
-    private int[] nexCol;
+  
 
     public KnightBoard(int startingRows,int startingCols) {
 
@@ -32,8 +31,8 @@ public class KnightBoard {
 	if((board.length * board[0].length)<=level){
 	    return true;}
 
-	if (row<0 || row >= board.length){return false;}
-	    if (col<0 || col>= board.length) {return false;}
+	if (row<0 || row >= board.length || col<0 || col>= board[0].length){return false;}
+	    
        
 	if (board[row][col]==0){
 
@@ -45,13 +44,13 @@ public class KnightBoard {
 		    return true;}		        
 	    }
        
-	     if(row > 1 && col < board.length -1){
+	     if(row > 1 && col < board.length ){
                	if(solveH(row-2,col+1,level+1)){
 
 		    return true;}
 	    }
        
-	     if(row <  board.length -2 && col <  board.length -1){
+	     if(row <  board.length -2 && col <  board.length){
                	if(solveH(row+2,col+1,level+1)){
 		    return true;}
 	    }
@@ -85,7 +84,6 @@ public class KnightBoard {
 	    }
 	}
     
-	System.out.println("no match");
 	return false;
     }
 
@@ -96,7 +94,7 @@ public class KnightBoard {
 	for (int i = 0; i < board.length ; i++) {
 	    for (int j=0; j < board[i].length; j++) {
 		if (board[i][j]> 99){	    
-		    fin += "" + board[i][j]+ " ";}
+		    fin += "" + board[i][j]+ "";}
 		if (board[i][j]>9){
 		    fin += " " + board[i][j] + " ";}
 		else{ fin += "  " + board[i][j] + " ";}
@@ -108,9 +106,21 @@ public class KnightBoard {
 
     
     public static void main (String[]args){
-	KnightBoard k = new KnightBoard(5,5);
+	KnightBoard k = new KnightBoard(5,6);
 	k.solve();
 	System.out.println(k.toString());
+	KnightBoard l = new KnightBoard(8,8);
+	l.solve();
+	System.out.println(l.toString());
+
+	KnightBoard n = new KnightBoard(9,9);
+	n.solve();
+	System.out.println(n.toString());
+	KnightBoard o = new KnightBoard(14,14);
+	o.solve();
+	System.out.println(o.toString());
+
+	
 
 	
     }
