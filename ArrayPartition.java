ca63cc0b9fa898f5b8c748902379b124a59ecaec
janjantjan <janjan.tjan@gmail.com>
2017-03-12 17:15:52 -0400

    NEW

---
+++ ArrayPartition.java
@@ -0,0 +1,19 @@
+public class ArrayPartition {
+
+    public int[] partition(int[] ary, int i){
+	
+       
+
+
+
+    }
+
+    public String makeString(int [] ary){
+	String fin = "";
+	for (int i=0; i < ary.length; i++){
+	    fin += ary[i];
+	}
+	return fin;
+    }  
+
+}
