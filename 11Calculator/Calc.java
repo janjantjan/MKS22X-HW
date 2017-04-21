24a572f8c6510926e01b994027baee035adb9f5f
janjantjan <janjan.tjan@gmail.com>
2017-04-21 14:49:20 -0400

    WTH is this percision error

--- 11Calculator/Calc.java
+++ 11Calculator/Calc.java
@@ -34,7 +34,7 @@ public class Calc{
 	Stack t = new Stack();
 	String[]str = s.split(" ");
 	for (int i = 0; i < str.length; i++){
-	    String temp = str.charAt(0) + "";
+	    String temp = str.get(0) + "";
 	    if (isOp(temp)){
 		int a = t.pop();
 		int b = t.pop();
