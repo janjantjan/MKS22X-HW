17dd227d85a036299f8e0c07e6f1a03cfef38b11
janjantjan <janjan.tjan@gmail.com>
2017-05-20 21:09:04 -0400

    did I forget to commit?

--- 14Median/MyHeap.java
+++ 14Median/MyHeap.java
@@ -71,7 +71,7 @@ public class MyHeap {
 		return true;
 	    }
 	    else{return false;}}
-	else{if (x>next){
+	else{if (x<next){
 		return false;
 	    }
 	    else{return true;}}
@@ -81,7 +81,7 @@ public class MyHeap {
 	int end = heap.size()-1;
 	int ending = heap.get(end);
 	int moving = heap.get(end/2);
-	while(toogreat(ending, moving)){
+	while(toogreat(ending, moving) && (end/2)!= 0){
 	    swap(end, end/2);
 	    if(end >= 1){
 		end = end/2;
@@ -99,34 +99,66 @@ public class MyHeap {
     }
 
     
-    private void  pushdown(){
-	int top = 1;
-	while (top*2 < heap.size()-1){
+    private void pushdown(){
+	if (max){
+	    int top = 1;
+	    while (top*2 < heap.size()-1){
+		System.out.println(makeString());
+		int val = heap.get(top);
+		if (heap.get(top*2)< heap.get(top*2+1)){
+		    if (heap.get(top*2)<heap.get(top)){
+			swap (top, top*2);
+			top = top*2;}
+		}
+		else {
+		    if (heap.get(top*2+1)<heap.get(top)){
+			swap (top, top*2+1);
+			top = top*2+1;}
+		}
+	    }	
+	    if (heap.get(top*2)<heap.get(top)){
 	    
-	    int val = heap.get(top);
-	    if (heap.get(top*2)< heap.get(top*2+1)){
-		if (heap.get(top*2)<heap.get(top)){
-		    swap (top, top*2);
-		    top = top*2;}
-	    }
-	    else {
-		if (heap.get(top*2+1)<heap.get(top)){
-		    swap (top, top*2+1);
-		    top = top*2+1;}
-	    }
-	}	
-	if (heap.get(top*2)<heap.get(top)){
-	    
-	    swap (top, top*2);
-	    top = top*2;
-	    return;}
+		swap (top, top*2);
+		top = top*2;
+		return;}
     
 	    
-	else if (heap.get(top*2+1)<heap.get(top)){
+	    else if (heap.get(top*2+1)<heap.get(top)){
 	    
-	    swap (top, top*2+1);
-	    top = top*2+1;
-	    return;}
+		swap (top, top*2+1);
+		top = top*2+1;
+		return;}
+	}
+	else{
+	    int top = 1;
+	    while (top*2 > heap.size()){
+	    
+		int val = heap.get(top);
+		if (heap.get(top*2)> heap.get(top*2+1)){
+		    if (heap.get(top*2)>heap.get(top)){
+			swap (top, top*2);
+			top = top*2;}
+		}
+		else {
+		    if (heap.get(top*2+1)>heap.get(top)){
+			swap (top, top*2+1);
+			top = top*2+1;}
+		}
+	    }	
+	    if (heap.get(top*2)>heap.get(top)){
+	    
+		swap (top, top*2);
+		top = top*2;
+		return;}
+    
+	    
+	    else if (heap.get(top*2+1)>heap.get(top)){
+		
+		swap (top, top*2+1);
+		top = top*2+1;
+		return;}
+	}
+	    
 	
     }
 	
@@ -172,21 +204,18 @@ private String makeString(){
 
      public static void main(String[]HELLOOOOOO){
 	 MyHeap a = new MyHeap(false);
-	 a.add(5);
-	 a.add(7);
-	 a.add(2);
-	 a.add(9);
-	 a.add(0);
-	 a.add(4);
+
+	 a.add(3);
+	 a.add(3);
 	 a.add(1);
-	 a.add(555);
-	 a.add(7);
-	 a.add(2);
-	 a.add(9);
-	 a.add(0);
-	 a.add(4);
 	 a.add(1);
-	 a.add(555);
+	 a.add(1);
+	 a.add(1);
+	 a.add(2);
+	 a.add(1);
+	 a.add(1);
+	 a.add(0);
+	 a.add(2);
 
 	
 	 System.out.println(a.makeString());
