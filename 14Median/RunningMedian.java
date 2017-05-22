a9121c8d4b257a98a71e25b31c1dd2580b0f43fd
janjantjan <janjan.tjan@gmail.com>
2017-05-21 23:39:22 -0400

    has some issues

--- 14Median/RunningMedian.java
+++ 14Median/RunningMedian.java
@@ -1,3 +1,4 @@
+import java.lang.*;
 public class RunningMedian{
     
     MyHeap less;
@@ -10,16 +11,36 @@ public class RunningMedian{
 	
 
     public void add(int num){
-	if (less.size()> more.size()){
+	if (less.size() == 0) {
+	    less.add(num);
+	    return;}
+	
+	if (more.size() == 0) {
 	    more.add(num);
-	}
-	if (more.size()>less.size()){
+	    return;}
+
+	
+	double median = getMedian();
+	if (num > median){
 	    less.add(num);
 	}
-	else{ more.add(num);}
+	else{
+	    more.add(num);}
+
+	 if (Math.abs(less.size() - more.size()) > 1){
+	     if (less.size()> more.size()){
+	 	int move = less.remove();
+	 	more.add(move);
+	     }
+	     else{ int moves = more.remove();
+	 	less.add(moves);
+	     }
+	 }
+    
     }
 
     public double getMedian(){
+	
 	if(more.size()== less.size()){
 	    int a = less.peek();
 	    int b = more.peek();
@@ -31,6 +52,30 @@ public class RunningMedian{
 	}
 	else{ return 0.0 + less.peek();}
     }
+
+    private String printMore(){
+	return more.makeString();
+    }
+
+   private String printLess(){
+	return less.makeString();
+    } 
+
+    public static void main (String[]argss){
+	RunningMedian a = new RunningMedian();
+	a.add(1);
+	a.add(2);
+	a.add(4);
+	a.add(5);//
+	a.add(9);
+	a.add(11);
+	a.add(100);
+							
+	System.out.println(a.getMedian());
+	System.out.println(a.printMore());
+	System.out.println(a.printLess());
+    }
+    
 }
 
     
