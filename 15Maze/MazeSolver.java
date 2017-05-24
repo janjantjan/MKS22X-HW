32a8b9ab66bb50105cd879d3a4f88b2ea42d8981
janjantjan <janjan.tjan@gmail.com>
2017-05-23 22:18:13 -0400

    OMG it worksss... minus A*..

--- 15Maze/MazeSolver.java
+++ 15Maze/MazeSolver.java
@@ -20,10 +20,11 @@ public class MazeSolver{
 	Location end = board.getEnd();
 	int startR = start.getRow();
 	int startC = start.getCol();
+	//System.out.println("startR: " + startR + " startC: " + startC);
 	int endR = end.getRow();
 	int endC = end.getCol();
 	int toGoal = (int) Math.abs(startR - endR) + Math.abs(startC - endC);
-	System.out.println("A");
+	//System.out.println("A");
 	//Frontier front;
 	
 	if (style == 0){
@@ -33,7 +34,7 @@ public class MazeSolver{
 	if (style == 1){
 	    front = new FrontierQueue();
 	    front.add(start);
-	    System.out.println("C");
+	    //System.out.println("C");
 	}
 	if (style == 2){
 	    front = new FrontierPriorityQueue();
@@ -42,26 +43,27 @@ public class MazeSolver{
 	// if (style == 3){
 	// } ???
 	
-	System.out.println("B");
+	//System.out.println("B");
 	 
 	while( front.size() > 0){
 	    Location current = front.next();
 	    int currentR = current.getRow();
 	    int currentC = current.getCol();
-	    System.out.println("D");
-	    if ((currentR == startR) && (currentC == currentR)){
+	    //System.out.println("D");
+	    if ((currentR == endR) && (currentC == endC)){
 		//set(currentR, currentC, '@');
 		while(front.size() > 0){
 		    Location next = front.next();
 		    int nextR = next.getRow();
 		    int nextC = next.getCol();
 		    board.set(nextR, nextC, '@');
-		    if (animate) {System.out.println(toString());}
+		    if (animate) {
+			System.out.println(toString());}
 		}
 	    }
-		
+	    //System.out.println("E");
 	    int[] neighbors = neigh(current);
-	    System.out.println(emptyary(neighbors));
+	    //System.out.println(emptyary(neighbors));
 	    if (neighbors[0]==1){//row, col-1)
 		board.set(currentR, currentC-1, '.');
 		if (animate) {System.out.println(toString());}
@@ -103,23 +105,28 @@ public class MazeSolver{
     private int[] neigh(Location x){
 	int row = x.getRow();
 	int col = x.getCol();
+	//System.out.println("row: " + row + " col: " + col);
 	int[] fin = new int[4]; // ( L, R, U, D)
 	if (col > 0){
+	    //System.out.println("w");
 	    if (board.get(row, col-1) == ' '){
 		fin[0] = 1;
 	    }
 	    else{fin[0] = 0;}}
 	if (col < board.getmaxC()){
+	    //System.out.println("x");
 	    if (board.get(row, col+1) == ' ' ){
 		fin[1] = 1;
 	    }
 	    else{fin[1] = 0;}}
 	if (row > 0 ){
+	    //System.out.println("y");
 	    if (board.get(row-1, col) == ' ' ){
 		fin[2] = 1;
 	    }
 	    else{fin[2] = 0;}}
 	if (row < board.getmaxR()){
+	    //System.out.println("z");
 	    if (board.get(row+1, col) == ' ' ){
 		fin[3] = 1;
 	    }
@@ -136,8 +143,9 @@ public class MazeSolver{
     }
 	  
     public String toString(){
-        return board.toString();}
-    
+	if(animate){return board.toString(50);}
+	else{        return board.toString();}
+    }
 			    
     public static void main (String[]args){
 
